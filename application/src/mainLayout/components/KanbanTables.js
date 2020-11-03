20ecbbc41e8af71758232a95e344618497a09f75
szal03 <33811775+szal03@users.noreply.github.com>
2020-11-03 12:08:38 +0100

    kanbans tasks table and style

--- application/src/mainLayout/components/KanbanTables.js
+++ application/src/mainLayout/components/KanbanTables.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import SingleTables from './SingleTables';
+import '../componentsStyle/KanbanTables.css'
 
 const KanbanTables = (props) => {
     const userKanbanListButtonBackHandler = props.userKanbanListButtonBackHandler;
     const kanbanTablesContent = props.kanbanTablesContent;
     console.log(kanbanTablesContent);
     return (
-        <div>
-            <div>
+        <div className="kanbanTablesStyle">
+            <div className="backButtonContainer">
                 <button onClick={() => userKanbanListButtonBackHandler()}
                     style={{ float: 'left' }}>
                     <i className="fa fa-arrow-left"></i>
                 </button>
+            </div>
+            <div className="contentTitle">
+                <span>Kanban Name</span>
             </div>
             <div className="kanbanTablesContainer">
                 <SingleTables kanbanTablesContent={kanbanTablesContent} />
