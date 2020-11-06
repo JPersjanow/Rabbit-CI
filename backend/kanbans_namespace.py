6a0247552202cfc1b54fcb714572c189cefe1d18
Jakub Persjanow <jakubpersjanow@gmail.com>
2020-11-06 21:06:31 +0100

    Imporved code quality of associated with kanban namespace

--- backend/kanbans_namespace.py
+++ backend/kanbans_namespace.py
@@ -1,19 +1,15 @@
 from flask_restplus import Resource, fields
 from flask import jsonify
-from glob import glob
-import xmltodict
 import os
 
-from tools.xml_tools import update_xml_attribute, create_xml_tree_for_kanban_config
+from tools.xml_tools import update_xml_attribute
 from tools.config_reader import ConfigReader
-from tools.kanbans_tools import KanbanFinder
+from tools.kanbans_tools import KanbanFinder, KanbanCreator
 from api import api
 
 ns = api.namespace('resources/kanbans', description='Operations related to kanban boards located in management module')
-kanban_update_model = api.model('Kanban Update', {
-    'name': fields.String(required=True, description='Kanban name')
-})
-kanban_create_model = api.clone('Kanban Creation', kanban_update_model, {
+kanban_model = api.model('Kanban Model', {
+    'name': fields.String(required=True, description='Kanban name'),
     'description': fields.String(required=True, description='Kanban description')
 })
 
@@ -23,43 +19,42 @@ config = ConfigReader()
 @ns.route('/')
 class KanbansAll(Resource):
     @api.response(200, "Kanban boards fetched")
+    @api.response(500, "Could not fetch Kanban boards info")
     def get(self):
         """Returns all kanban boards with info"""
-        all_kanbans = glob(f"{config.kanbans_directory}/*",recursive=True)
-        all_kanbans_info_list = []
-        print(all_kanbans)
-        for single_kanban_dir in all_kanbans:
-            single_kanban_config_file = os.path.join(single_kanban_dir, "config.xml")
-            with open(single_kanban_config_file) as xml_file:
-                all_kanbans_info_list.append(xmltodict.parse(xml_file.read()))
-        
-        response = jsonify(all_kanbans_info_list)
-        response.headers.add("Access-Control-Allow-Origin", "*")
+        kanban_finder = KanbanFinder()
+        try:
+            all_kanbans_info_list = kanban_finder.return_all_kanabans_info(kanbans_directory=config.kanbans_directory)
+            print(all_kanbans_info_list)
+            response = jsonify(all_kanbans_info_list)
+            print(response)
+            # response.headers.add("Access-Control-Allow-Origin", "*")
+        except Exception as e:
+            return {"response": f"Kanbans couldn't be fetched! {e}"}, 500
 
+        del(kanban_finder)
         return response
     
-    @ns.expect(kanban_create_model)
+    @ns.expect(kanban_model)
     @api.response(201, "New kanban board created")
     @api.response(500, "Kanban could not be created")
     def post(self):
         """Create new kanban"""
-        all_kanbans = glob(f"{config.kanbans_directory}/*",recursive=True)
+        kanban_creator = KanbanCreator()
         try:
-            new_kanban_dir = os.path.join(config.kanbans_directory, str(len(all_kanbans) + 1))
-            os.mkdir(new_kanban_dir,  mode=0o777)
+            new_kanban_dir, new_kanban_id= kanban_creator.create_new_kanban_folder(kanbans_directory=config.kanbans_directory)
         except FileExistsError:
             return {"response": "Kanban could not be created! Kanban already exists"}, 500
         except Exception as e:
             return {"response": "Kanban could not be created!", "exception": str(e)}, 500
         try:
-                config_tree = create_xml_tree_for_kanban_config(kanban_name=api.payload['name'], kanban_id=len(all_kanbans)+1, description=api.payload['description'])
-                with open(os.path.join(new_kanban_dir,"config.xml",), "w+") as file:
-                    file.write(config_tree)
+                config_tree = kanban_creator.create_xml_tree_for_kanban_config(kanban_name=api.payload['name'], kanban_id=new_kanban_id, description=api.payload['description'])
+                kanban_creator.create_new_kanban_config(new_kanban_directory=new_kanban_dir, config_xml_tree=config_tree)
         except Exception as e:
             os.rmdir(new_kanban_dir)
             return {"response": "Failed while creating config.xml file, deleting kanban.", "exception": str(e)}, 500
 
-        return {"response": f"New kanban board with id {len(all_kanbans) + 1} created"}
+        return {"response": f"New kanban board with id {new_kanban_id} under {new_kanban_dir} created"}, 201
         
 
 @ns.route('/<int:kanban_id>')
@@ -73,26 +68,22 @@ class KanbanSingle(Resource):
         kanban_directory, kanban_found = kanban_finder.find_kanban_dir_with_id(kanban_id=kanban_id, kanbans_directory=config.kanbans_directory)
         
         if kanban_found:
-            kanban_config_file = os.path.join(kanban_directory, "config.xml")
-            with open(kanban_config_file) as xml_file:
-                kanban_info_list = xmltodict.parse(xml_file.read())
+            kanban_info_list = kanban_finder.return_kanban_info(kanban_directory=kanban_directory)
             response = jsonify(kanban_info_list)
             response.headers.add("Access-Control-Allow-Origin", "*")
             return response
         else:
             return {"response": f"Kanban board with id {kanban_id} not found"}, 404
 
-    @ns.expect(kanban_create_model)
+    @ns.expect(kanban_model)
     # @ns.marshal_with(kanban) THIS IS RETURNED
     def put(self, kanban_id):
-        """Updates name of kanban board with given id"""
+        """Updates kanban board with given id"""
         kanban_finder = KanbanFinder()
         kanban_directory, kanban_found = kanban_finder.find_kanban_dir_with_id(kanban_id=kanban_id, kanbans_directory=config.kanbans_directory)
-        print(kanban_directory)
         response = dict()
         if kanban_found:
             config_file_dir = os.path.join(kanban_directory, 'config.xml')
-            print(config_file_dir)
             try:
                 if api.payload['name'] != 'string':
                     update_xml_attribute(config_file_dir, 'name', api.payload['name'])
@@ -102,9 +93,9 @@ class KanbanSingle(Resource):
                     response["response_desc"] = f"Updated with {api.payload['description']}"
             except KeyError:
                 return {"response": "Wrong key! Use given model"}, 404
+            finally:
+                del(kanban_finder)
             
-
-            del(kanban_finder)
             return response, 201
 
         else:
