e71bee2dac49f75e46299e6651a41176544c4dbf
Jakub Persjanow <jakubpersjanow@gmail.com>
2020-11-11 11:13:49 +0100

    Updated API version, added logging to kanban namespace

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,7 @@ from flask_restplus import Api
 from tools.log import setup_custom_logger
 
 api = Api(
-    version="0.32",
+    version="0.33",
     title="Rabbit-CI API",
     description="This is a REST API for Rabbit-CI",
 )
